2ddf06dbb64e3f18f051e267ec5e1f1b64a0d557
Aniket Datta <datta.aniket7@gmail.com>
2023-09-04 01:16:35 +0530

    async-await

---
+++ async-await.js
@@ -0,0 +1,47 @@
+const random = () => {
+  return Promise.resolve(Math.random());
+};
+
+// const sumRandomAsyncNums = () => {
+//   let first;
+//   let second;
+//   let third;
+
+//   return random()
+//     .then((v) => {
+//       first = v;
+//       return random();
+//     })
+//     .then((v) => {
+//       second = v;
+//       return random();
+//     })
+//     .then((v) => {
+//       third = v;
+//       return first + second + third;
+//     })
+//     .then((v) => {
+//       console.log(`Result ${v}`);
+//     });
+// };
+
+const sumRandomAsyncNums = async () => {
+  const first = await random();
+  const second = await random();
+  const third = await random();
+
+  console.log(`Result ${first + second + third}`);
+
+  if (await random()) {
+    // do something
+  }
+
+  const randos = Promise.all([random(), random(), random()]);
+  console.log(randos);
+
+  for (const r of await randos) {
+    console.log(r);
+  }
+};
+
+sumRandomAsyncNums();
