3a1a922d2f4ce5396a1dd38f1f5b35734d6ab874
Aniket Datta <datta.aniket7@gmail.com>
2023-09-03 23:48:42 +0530

    console-log

---
+++ consolelog.js
@@ -0,0 +1,32 @@
+const foo = { name: "tom", age: 30, nervous: false };
+const bar = { name: "dick", age: 40, nervous: false };
+const baz = { name: "harry", age: 50, nervous: true };
+
+console.log(foo);
+console.log(bar);
+console.log(baz);
+
+// Computed Property Names
+
+console.log("%c My Frieds", "color: orange; font-weight: bold;");
+console.log({ foo, bar, baz });
+
+// Console.table(...)
+console.table([foo, bar, baz]);
+
+//Console.time
+console.time("looper");
+
+let i = 0;
+while (i < 1000000) {
+  i++;
+}
+
+console.timeEnd("looper");
+
+//Stack Trace Logs
+
+const deleteMe = () => console.trace("bye bye database");
+
+deleteMe();
+deleteMe();
